d6fb23647cae3e817a1682db8712101e209de5ed
dydy <dydy@piffko.com>
2021-09-24 22:37:35 +0200

    username and password validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,13 @@ const userModel = require("../models/user.model.js");
 const createError = require('../errors/error.js');
 
 function register (req, res, next) {
+	let {username, password} = req.body;
+	if(username.length < 6 || username.length > 20){
+		next(createError('INVALID_USERNAME'));
+	}
+	if(!password.match(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*?]).{8,}/g)){
+		next(createError('INVALID_PASSWORD'));
+	}
 	bcrypt.hash(req.body.password, 10, (err, hash) => {
 		if(err){
 			next(createError(0));
@@ -12,7 +19,7 @@ function register (req, res, next) {
 			password: hash
 		});
 		user.register().then(msg => {
-			res.status(200).send(msg);
+			res.status(200).json({message: msg});
 		}).catch(error => {
 			next(error);
 		})
--- errors/error.js
+++ errors/error.js
@@ -1,6 +1,14 @@
 function createError(code) {
 	const error = new Error();
 	switch (code) {
+		case 'INVALID_USERNAME':
+			error.status = 400;
+			error.message = 'Invalid username (must contain 6-20 characters)';
+			break;
+		case 'INVALID_PASSWORD':
+			error.status = 400;
+			error.message = 'Invalid password (must contain at least 8 characters, lowercase, uppercase, number and special character)';
+			break;
 		default:
 			error.status = 500;
 			error.message = 'Internal server error';
